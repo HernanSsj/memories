9156e623e5a9ce244885394c2484d6aecd2ea8ea
HernanSsj <hernanns46gmail.com>
2020-12-23 16:22:23 -0300

    final

--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ import morgan from 'morgan'
 import dotenv from "dotenv"
 const app = express()
 dotenv.config()
-app.use(morgan('tiny'))
+
 app.use(bodyParser.json({limit:"30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}))
 app.use(cors());
+
 app.use("/posts", postRoutes)
+
 app.get("/", (req, res) =>{
     res.send("Recuerdos API")
 })
@@ -19,7 +21,7 @@ const CONECCTION_URL = process.env.CONECCTION_URL
 const PORT = process.env.port || 5000
 
 mongoose.connect(CONECCTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then( app.listen(PORT, console.log(`Servidor escuchando en el puerto: ${PORT}`)))
+    .then( app.listen(PORT, "0.0.0.0" ,console.log(`Servidor escuchando en el puerto: ${PORT}`)))
     .catch( (error) => console.log(error.message))
 
 mongoose.set("useFindAndModify", false)
